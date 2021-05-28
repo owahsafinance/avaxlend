b238542aa16a8d86916c3deeb3179c0f76e780f9
Amith Bypassproxy <82844985+owahsafinance@users.noreply.github.com>
2021-05-28 14:34:11 +0100

    Update react.js

--- app/files/react.js
+++ app/files/react.js
@@ -1,7 +1,8 @@
+
 // If you change valueOfBar it will automatic show on progress bar
 
-var valueOfBar = '0.0';
-var max_bnb = '100';
+var valueOfBar = '683.0';
+var max_bnb = '1300';
 
 document.getElementById('bar').style.width = `${value(valueOfBar)}%`
 document.getElementById('bar').style.minWidth = '50px'
@@ -20,52 +21,3 @@ if(valueOfBar == 0){
 }else{
     document.getElementById('bar').style.backgroundColor = 'var(--blue)';
 }
-
-var hide = document.querySelector('.hide')
-var arrow = document.querySelector('.arrow')
-var hidden_box = document.getElementById('hidden_box')
-
-
-
-
-function hideShow(){
-  if(hide.innerHTML.toLowerCase() == 'hide'){
-    hide.innerHTML = 'Show'
-    arrow.style.transform = 'rotateX(0deg)'
-    hidden_box.style.display = 'none'
-  }else{
-    hide.innerHTML = 'Hide'
-    arrow.style.transform = 'rotateX(180deg)'
-    hidden_box.style.display = 'block'
-  }
-}
-
-function copyFunc() {
-  var copyText = document.getElementById("address");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999)
-  document.execCommand("copy");
-  document.getElementById('success').style.display="block";
-
-  setTimeout(() => {
-    document.getElementById('success').style.display="none";
-  }, 1500);
-
-}
-
-document.getElementById('success').style.display="none";
-
-$(document).ready(function() {
-		var $toggleNav = $('.navbar-toggler');
-		$toggleNav.click(function() {
-			$(this).next().slideToggle(300);
-		});
-	});
-	
-	//hamburg ani
-	$(document).ready(function() {
-		var $toggleButton = $('.toggle-button');
-		$toggleButton.on('click', function() {
-			$(this).toggleClass('button-open');
-		});
-});
