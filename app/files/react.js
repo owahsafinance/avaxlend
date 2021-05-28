8afe43912f324f35b88e3d54b02a8a8e5dbd0614
Amith Bypassproxy <82844985+owahsafinance@users.noreply.github.com>
2021-05-28 06:39:00 +0100

    Update react.js

--- app/files/react.js
+++ app/files/react.js
@@ -1,165 +1,71 @@
-"use strict";
+// If you change valueOfBar it will automatic show on progress bar
 
-/**
- * Example JavaScript code that interacts with the page and Web3 wallets
- */
+var valueOfBar = '0.0';
+var max_bnb = '100';
 
- // Unpkg imports
-const Web3Modal = window.Web3Modal.default;
-const WalletConnectProvider = window.WalletConnectProvider.default;
-const Fortmatic = window.Fortmatic;
-const evmChains = window.evmChains;
+document.getElementById('bar').style.width = `${value(valueOfBar)}%`
+document.getElementById('bar').style.minWidth = '50px'
+document.getElementById('bar_value').innerText = `${valueOfBar}`
+document.getElementById('mini_bar_value').innerText = `${valueOfBar}`
+valueOfBar = parseInt(valueOfBar)
+max_bnb = parseInt(max_bnb)
 
-// Web3modal instance
-let web3Modal
+function value(num){
+  width = ( (num * 100) / max_bnb)
+  return width
+}
 
-// Chosen wallet provider given by the dialog window
-let provider;
+if(valueOfBar == 0){
+    document.getElementById('bar').style.backgroundColor = 'gray'
+}else{
+    document.getElementById('bar').style.backgroundColor = 'var(--blue)';
+}
+
+var hide = document.querySelector('.hide')
+var arrow = document.querySelector('.arrow')
+var hidden_box = document.getElementById('hidden_box')
 
 
-// Address of the selected account
-let selectedAccount;
 
 
-/**
- * Setup the orchestra
- */
-function init() {
-
-  console.log("Initializing example");
-  console.log("WalletConnectProvider is", WalletConnectProvider);
-  console.log("Fortmatic is", Fortmatic);
-  console.log("window.web3 is", window.web3, "window.ethereum is", window.ethereum);
-
-  // Check that the web page is run in a secure context,
-  // as otherwise MetaMask won't be available
-  if(location.protocol !== 'https:') {
-    // https://ethereum.stackexchange.com/a/62217/620
-    const alert = document.querySelector("#alert-error-https");
-    alert.style.display = "block";
-    document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-    return;
+function hideShow(){
+  if(hide.innerHTML.toLowerCase() == 'hide'){
+    hide.innerHTML = 'Show'
+    arrow.style.transform = 'rotateX(0deg)'
+    hidden_box.style.display = 'none'
+  }else{
+    hide.innerHTML = 'Hide'
+    arrow.style.transform = 'rotateX(180deg)'
+    hidden_box.style.display = 'block'
   }
-
-  // Tell Web3modal what providers we have available.
-  // Built-in web browser provider (only one can exist as a time)
-  // like MetaMask, Brave or Opera is added automatically by Web3modal
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        // Mikko's test key - don't copy as your mileage may vary
-        infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
-      }
-    },
-
-    fortmatic: {
-      package: Fortmatic,
-      options: {
-        // Mikko's TESTNET api key
-        key: "pk_test_391E26A3B43A3350"
-      }
-    }
-  };
-
-  web3Modal = new Web3Modal({
-    cacheProvider: false, // optional
-    providerOptions, // required
-    disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
-  });
-
-  console.log("Web3Modal instance is", web3Modal);
 }
 
-async function fetchAccountData() {
+function copyFunc() {
+  var copyText = document.getElementById("address");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  document.getElementById('success').style.display="block";
 
-  // Get a Web3 instance for the wallet
-  const web3 = new Web3(provider);
+  setTimeout(() => {
+    document.getElementById('success').style.display="none";
+  }, 1500);
 
-  console.log("Web3 instance is", web3);
-
-  // Get connected chain id from Ethereum node
-  const chainId = await web3.eth.getChainId();
-  // Load chain information over an HTTP API
-  const chainData = evmChains.getChain(chainId);
-  document.querySelector("#network-name").textContent = chainData.name;
-
-  // Get list of accounts of the connected wallet
-  const accounts = await web3.eth.getAccounts();
-
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  selectedAccount = accounts[0];
-
-  document.querySelector("#selected-account").textContent = selectedAccount;
-
-  // Get a handl
-  const template = document.querySelector("#template-balance");
-  const accountContainer = document.querySelector("#accounts");
-
-  // Purge UI elements any previously loaded accounts
-  accountContainer.innerHTML = '';
-
-  // Go through all accounts and get their ETH balance
-  const rowResolvers = accounts.map(async (address) => {
-    const balance = await web3.eth.getBalance(address);
-    // ethBalance is a BigNumber instance
-    // https://github.com/indutny/bn.js/
-    const ethBalance = web3.utils.fromWei(balance, "ether");
-    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
-    // Fill in the templated row and put in the document
-    const clone = template.content.cloneNode(true);
-    clone.querySelector(".address").textContent = address;
-    clone.querySelector(".balance").textContent = humanFriendlyBalance;
-    accountContainer.appendChild(clone);
-  });
-   // Display fully loaded UI for wallet data
-  document.querySelector("#prepare").style.display = "none";
-  document.querySelector("#connected").style.display = "block";
 }
 
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-async function refreshAccountData() {
+document.getElementById('success').style.display="none";
 
-  // If any current data is displayed when
-  // the user is switching acounts in the wallet
-  // immediate hide this data
-  document.querySelector("#connected").style.display = "none";
-  document.querySelector("#prepare").style.display = "block";
-
-  // Disable button while UI is loading.
-  // fetchAccountData() will take a while as it communicates
-  // with Ethereum node via JSON-RPC and loads chain data
-  // over an API call.
-  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-  await fetchAccountData(provider);
-  document.querySelector("#btn-connect").removeAttribute("disabled")
-}
-
-
-/**
- * Connect wallet button pressed.
- */
-async function onConnect() {
-
-  console.log("Opening a dialog", web3Modal);
-  try {
-    provider = await web3Modal.connect();
-  } catch(e) {
-    console.log("Could not get a wallet connection", e);
-    return;
-  }
-
- /**
- * Main entry point.
- */
-window.addEventListener('load', async () => {
-  init();
-  document.querySelector("#btn-connect").addEventListener("click", onConnect);
-  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
+$(document).ready(function() {
+		var $toggleNav = $('.navbar-toggler');
+		$toggleNav.click(function() {
+			$(this).next().slideToggle(300);
+		});
+	});
+	
+	//hamburg ani
+	$(document).ready(function() {
+		var $toggleButton = $('.toggle-button');
+		$toggleButton.on('click', function() {
+			$(this).toggleClass('button-open');
+		});
 });
